022f361684290abff0cf9f9aef736328f9d13a38
Johan Kjölhede <kjolhede@telia.com>
2014-08-14 00:21:00 +0200

    began working on backen

--- mnet-backend/src/main/scala/se/culvertsoft/mnet/backend/BackEnd.scala
+++ mnet-backend/src/main/scala/se/culvertsoft/mnet/backend/BackEnd.scala
@@ -1,5 +1,21 @@
 package se.culvertsoft.mnet.backend
 
-class BackEnd {
+object BackEnd {
 
+  val DEFAULT_PORT = 6537
+
+  def main(args: Array[String]) {
+    //TODO: Parse args...maybe just a config file name?
+    val settings = new BackendConfiguration
+    new BackEnd(settings).run()
+  }
+
+}
+
+class BackEnd(settings: BackendConfiguration) {
+  val port = if (settings.hasPort) settings.getPort else BackEnd.DEFAULT_PORT
+  
+  def run() {
+  }
+  
 }
