bf9bc5ecc6b564617e341e79ef3f0663f68b62c7
Johan Kjölhede <kjolhede@telia.com>
2014-08-23 08:38:20 +0200

    adding include to <memory>

--- mnet-cppclient/src/main/cpp/mnet/MNetSerializer.h
+++ mnet-cppclient/src/main/cpp/mnet/MNetSerializer.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 #include <mgen/serialization/VectorInputStream.h>
 #include <mgen/serialization/VectorOutputStream.h>
 #include <mgen/serialization/JsonReader.h>
